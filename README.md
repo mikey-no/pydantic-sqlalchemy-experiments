c7ed92b5f0c3b35fb5f69a65ae69bbcd43b0de3b
Mikey-no <mikey-no@someexample.com>
2021-02-09 21:48:49 +0000

    new improved update method added (that does not use pydantic as much) and demonstration of buggy update function

--- README.md
+++ README.md
@@ -10,14 +10,30 @@
 
 
 ## Features of the application:
-- Has pytest
-- Uses an in memory sqlite database [1]
+- It tested with pytest [1]
+- Uses an in memory sqlite database [2]
 - Uses the SQLAlchemy declarative model definition, not the imperative definition as used by [Mr-Manna FastAPI CRUD](https://github.com/Mr-Manna/FastAPI-CRUD)
 - It just uses pip (simple as can be)
 - Automatically works out the name of each attribute to be updated
 
-This application started here [pydantic-sqlalchemy](https://github.com/tiangolo/pydantic-sqlalchemy).
-I am not fully satisfied with the comments I have added to the test_update function. I wanted to provide clear 
+### Feature: test_user_update() - using pydantic
+- uses pydantic to merge the two dictionaries
+- bit complicated
+
+### Feature: test_user_update_2() - using setattr(db_user, var, value)
+- uses the python `setattr` function [w3schools setattr](https://www.w3schools.com/python/ref_func_setattr.asp)
+- simpler
+- **preferred**
+
+### Feature: demonstrate a bug, use function test_user_update_3()
+- calls a function with the bug (would have to be uncommented to run)
+- pytest calls this method and 2 tests pass (as above), but the test associated with this bug fails
+
+
+This application started here [pydantic-sqlalchemy](https://github.com/tiangolo/pydantic-sqlalchemy). I was looking for
+a simple way to update a sqlalchemy model.
+
+I am not fully satisfied with the comments I have added to the test_user_update function. I wanted to provide clear 
 information about what is going on.
 
 # To setup
@@ -44,6 +60,8 @@ pytest main.py
 
 ## Notes
 
-[1] This does not work in a FastAPI application (I could not make this work). This same functionality is difficult to achieve. 
+- [1] pytest has better methods to initiate and clean up before and after each test. I have used none of them to 
+ensure this remains a single page application.
+- [2] This does not work in a FastAPI application (I could not make this work). This same functionality is difficult to achieve. 
 I used [timhuges - in memory database running test](https://github.com/timhughes/example-fastapi-sqlachemy-pytest) 
 to enable this functionality, but this is not included here (yet?).
--- main.py
+++ main.py
@@ -4,24 +4,26 @@ from typing import List, Optional
 from pydantic import BaseModel
 from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, relationship, sessionmaker
+from sqlalchemy.orm import Session, relationship, sessionmaker, exc
 from datetime import datetime
 
 Base = declarative_base()
 
-engine = create_engine("sqlite://", echo=False) # this does not work in FastAPI BTW, in memory sqlite db does not work
+engine = create_engine("sqlite://", echo=False)  # this does not work in FastAPI BTW, in memory sqlite db does not work
 
-create_now = datetime.utcnow() # fake created time - making testing consistency easier
-modified_now = datetime.utcnow() # fake last modified time
+create_now = datetime.utcnow()  # fake created time - making testing consistency easier
+modified_now = datetime.utcnow()  # fake last modified time
 
 # show extra print statements
 __SHOW__ = False
-
 
 """
 Sqlalchemy using declarative style model definitions and pydantic 
 without pydantic_sqlachemy
 """
+
+
+# normally models.py
 
 class User(Base):
     __tablename__ = "users"
@@ -31,7 +33,7 @@ class User(Base):
     fullname = Column(String)
     nickname = Column(String)
 
-    created = Column(DateTime(timezone=True), default=create_now) # func.now() instead of create_now
+    created = Column(DateTime(timezone=True), default=create_now)  # func.now() instead of create_now
     modified = Column(DateTime(timezone=True), onupdate=modified_now)
 
     addresses = relationship(
@@ -50,6 +52,8 @@ class Address(Base):
 
 # PydanticUser = sqlalchemy_to_pydantic(User)
 # PydanticAddress = sqlalchemy_to_pydantic(Address)
+
+# normally schema.py
 
 class PydanticUser(BaseModel):
     id: int
@@ -87,22 +91,115 @@ class PydanticUserWithAddresses(PydanticUser):
     addresses: List[PydanticAddress] = []
 
 
+# normally orm.py (for me)
+
 Base.metadata.create_all(engine)
 
 LocalSession = sessionmaker(bind=engine)
 
 db: Session = LocalSession()
 
-ed_user = User(name="ed", fullname="Ed Jones", nickname="edsnickname")
 
-address = Address(email_address="ed@example.com")
-address2 = Address(email_address="eddy@example.com")
-ed_user.addresses = [address, address2]
-db.add(ed_user)
-db.commit()
+# ------------------------------ Setup data ------------------------------
 
+def load_data():
+    ed_user = User(name="ed", fullname="Ed Jones", nickname="edsnickname")
+
+    address = Address(email_address="ed@example.com")
+    address2 = Address(email_address="eddy@example.com")
+    ed_user.addresses = [address, address2]
+    db.add(ed_user)
+    db.commit()
+
+
+# ------------------------------ Clean up data ------------------------------
+
+def drop_data():
+    """ drop all User and I assume address data as this should cascade delete (this bit has not been tested) """
+    db.query(User).delete()
+
+
+# ------------------------------ Updates ------------------------------
+
+def update_user(db: Session, user: PydanticUserUpdate):
+    """
+    Using a new update method seen in FastAPI https://github.com/tiangolo/fastapi/pull/2665
+    Simple, does not need each attribute to be updated individually
+    Uses python in built functionality... preferred to the pydintic related method above
+    """
+
+    # get the existing data
+    db_user = db.query(User).filter(User.id == user.id).one_or_none()
+    if db_user is None:
+        return None
+
+    # Update model class variable from requested fields # **typo** was vars(db_user) => vars(user)
+    for var, value in vars(user).items():
+        setattr(db_user, var, value) if value else None
+
+    db_user.modified = modified_now
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def update_user_with_pydantic(db: Session, user: PydanticUserUpdate):
+    """ Not the preferred method as it requires the use of Pydantic """
+
+    # make the query for the relevant record (needed later)
+    user_query = db.query(User)
+    # get the existing data
+    user_stored = user_query.first()
+    pydantic_user_stored = PydanticUser.from_orm(user_stored)
+    user_data = pydantic_user_stored.dict()
+    if __SHOW__: print(f'existing user: {user_data}')
+
+    user_updates_data = user.dict(exclude_unset=True)
+
+    # add in the modified datetime
+    user_updates_data["modified"] = modified_now
+
+    # create the merged dict with new updates overwriting existing data
+    updated_user_data = pydantic_user_stored.copy(update=user_updates_data)
+    if __SHOW__: print(f'updated_user_data: {updated_user_data.dict()}')
+
+    # add the update to the db - re-use the same query
+    user_query.update(updated_user_data)
+    db.commit()
+    db.refresh(user_stored)
+    return user_stored
+
+
+def update_user_bug(db: Session, user: PydanticUserUpdate):
+    """
+    BUG!!
+    Using a new update method seen in FastAPI https://github.com/tiangolo/fastapi/pull/2665
+    Simple, does not need each attribute to be updated individually
+    Uses python in built functionality... preferred to the pydintic related method above
+    """
+
+    # get the existing data
+    db_user = db.query(User).filter(User.id == user.id).one_or_none()
+    if db_user is None:
+        return None
+
+    # Bug demoed here!!!
+    # Update model class variable from requested fields # **typo** was vars(db_user) => vars(user)
+    for var, value in vars(db_user).items():
+        setattr(db_user, var, value) if value else None
+
+    db_user.modified = modified_now
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+# ------------------------------ Tests ------------------------------
 
 def test_normal_orm():
+    load_data()
     user = db.query(User).first()
     pydantic_user = PydanticUser.from_orm(user)
     data = pydantic_user.dict()
@@ -128,49 +225,27 @@ def test_normal_orm():
             {"email_address": "eddy@example.com", "id": 2, "user_id": 1},
         ],
     }
+    drop_data()
 
 
 def demo():
+    load_data()
     user = db.query(User).first()
     pydantic_user = PydanticUser.from_orm(user)
     data = pydantic_user.dict()
     print(data)
+    drop_data()
 
 
 def test_user_update():
-    """ Based on the FastAPI sqlalchemy demo app that does not include the update operation :( """
-    print('\n')
-    # make the query for the relevant record (needed later)
-    user_query = db.query(User)
-    # get the existing data
-    user_stored = user_query.first()
-    pydantic_user_stored = PydanticUser.from_orm(user_stored)
-    user_data = pydantic_user_stored.dict()
-    if __SHOW__: print(f'existing user: {user_data}')
-    assert user_data == {
-        "fullname": "Ed Jones",
-        "id": 1,
-        "name": "ed",
-        "nickname": "edsnickname",
-        "created": create_now,
-        "modified": None
-    }
+    """
+    Based on the FastAPI sqlalchemy demo app Jan 2021
+    """
+    load_data()
 
     # make the pydantic update model (done in router.py normally)
-    user_updates_pydantic = PydanticUserUpdate(name='fred', nickname='python')
-    user_updates_data = user_updates_pydantic.dict(exclude_unset=True)
-
-    # add in the modified datetime
-    user_updates_data["modified"] = modified_now
-
-    # create the merged dict with new updates overwriting existing data
-    updated_user_data = pydantic_user_stored.copy(update=user_updates_data)
-    if __SHOW__: print(f'updated_user_data: {updated_user_data.dict()}')
-
-    # add the update to the db - re-use the same query
-    user_query.update(updated_user_data)
-    db.commit()
-    db.refresh(user_stored)
+    user_updates_pydantic = PydanticUserUpdate(name='fredzz', nickname='python')
+    updated_user = update_user_with_pydantic(db, user_updates_pydantic)
 
     # load the result back from the db and and test it has taken the results and not lost unchanging values
     user_stored_res = db.query(User).first()
@@ -182,10 +257,98 @@ def test_user_update():
         'modified': modified_now,
         'fullname': 'Ed Jones',
         'id': 1,
-        'name': 'fred',
+        'name': 'fredzz',
         'nickname': 'python'
     }
 
+    drop_data()
+
+
+def test_user_update_2():
+    """
+    Using a new update method seen in FastAPI https://github.com/tiangolo/fastapi/pull/2665 8 Feb 2021
+    Not strictly using pydantic to do the update; although this is needed to create the data
+    for the update and transform it back for testing purposes
+    """
+    load_data()
+    start_user = db.query(User).first()
+
+    assert PydanticUser.from_orm(start_user).dict() == {'id': 1,
+                                                        'name': 'ed',
+                                                        'fullname': 'Ed Jones',
+                                                        'nickname': 'edsnickname',
+                                                        'created': create_now,
+                                                        'modified': None
+                                                        }
+    if __SHOW__: print(f'Before:   {PydanticUser.from_orm(start_user).dict()}')
+
+    user_updates_pydantic = PydanticUserUpdate(id=1, name='fred')
+    assert PydanticUserUpdate.from_orm(user_updates_pydantic).dict() == {'id': 1,
+                                                                         'name': 'fred',
+                                                                         'fullname': None,
+                                                                         'nickname': None,
+                                                                         'created': None
+                                                                         }
+    if __SHOW__: print(f'Updates:  {PydanticUserUpdate.from_orm(user_updates_pydantic).dict()}')
+    updated_user = update_user(db, user_updates_pydantic)
+
+    if updated_user is None:
+        print(f'Error: user id: {user_updates_pydantic.id} not found to update')
+
+    assert PydanticUser.from_orm(updated_user).dict() == {'id': 1,
+                                                          'name': 'fred',
+                                                          'fullname': 'Ed Jones',
+                                                          'nickname': 'edsnickname',
+                                                          'created': create_now,
+                                                          'modified': modified_now
+                                                          }
+    if __SHOW__: print(f'After:    {PydanticUser.from_orm(updated_user).dict()}')
+    drop_data()
+
+def test_user_update_3():
+    """
+    BUG!!
+    Using a new update method seen in FastAPI https://github.com/tiangolo/fastapi/pull/2665 8 Feb 2021
+    Not strictly using pydantic to do the update; although this is needed to create the data
+    for the update and transform it back for testing purposes
+    """
+    load_data()
+    start_user = db.query(User).first()
+
+    assert PydanticUser.from_orm(start_user).dict() == {'id': 1,
+                                                        'name': 'ed',
+                                                        'fullname': 'Ed Jones',
+                                                        'nickname': 'edsnickname',
+                                                        'created': create_now,
+                                                        'modified': None
+                                                        }
+    if __SHOW__: print(f'Before:   {PydanticUser.from_orm(start_user).dict()}')
+
+    user_updates_pydantic = PydanticUserUpdate(id=1, name='fred')
+    assert PydanticUserUpdate.from_orm(user_updates_pydantic).dict() == {'id': 1,
+                                                                         'name': 'fred',
+                                                                         'fullname': None,
+                                                                         'nickname': None,
+                                                                         'created': None
+                                                                         }
+    if __SHOW__: print(f'Updates:  {PydanticUserUpdate.from_orm(user_updates_pydantic).dict()}')
+    updated_user = update_user_bug(db, user_updates_pydantic)
+
+    if updated_user is None:
+        print(f'Error: user id: {user_updates_pydantic.id} not found to update')
+
+    assert PydanticUser.from_orm(updated_user).dict() == {'id': 1,
+                                                          'name': 'fred',
+                                                          'fullname': 'Ed Jones',
+                                                          'nickname': 'edsnickname',
+                                                          'created': create_now,
+                                                          'modified': modified_now
+                                                          }
+    if __SHOW__: print(f'After:    {PydanticUser.from_orm(updated_user).dict()}')
+    drop_data()
+
 
 if __name__ == "__main__":
-    test_user_update() # when you do not want to call via pytest
+    test_user_update()  # when you do not want to call via pytest
+    test_user_update_2()
+    # test_user_update_3() # bug method
